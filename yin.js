8050e2ae60e064e881bf199781b841f68c124b5f
liushang233 <664913676@qq.com>
2017-09-19 17:45:27 +0800

    1

---
+++ yin.js
@@ -0,0 +1,33 @@
+const request = require('request');
+const fs = require('fs');
+
+(() => {
+	const yinUrl = 'https://yys.tongren.163.com/article/?sort=new&span=30&tags=Cosplay&tags=%E5%90%8C%E4%BA%BACOS&start=0';
+	getUrl(yinUrl);
+})();
+
+function getUrl(yinUrl) {
+	request({
+		url: yinUrl,
+		headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+            'Referer': 'http://yys.163.com/tongren/'
+        }
+	},function(err, res, body) {
+		if(!err && res.statusCode === 200){
+			let result = JSON.parse(body).data.articles;
+			console.log(result.length);
+			for(let i = 0; i< result.length; i++){
+				let url = result[i].body[0].fp_data.url;
+				let title = `${result[i].id}`;
+				savePic(url, title);
+			}
+		}
+	})
+}
+
+function savePic(url, title){
+	console.log(`存储图片-->${title}`);
+	request(url).pipe(fs.createWriteStream(`./gif/${title}.jpg`));
+	console.log(`图片${title}-->存储完成啦！`);
+}
